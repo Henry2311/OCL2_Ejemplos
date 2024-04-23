7f2b293b204f75d33cd221637ed9e56a13e47acc
hmendozaGrupoexpande <hmendoza@grupoasys.com>
2024-04-22 19:11:30 -0600

    Traduccion a RISC-V 3

---
+++ Clase_10_11/instrucciones.py
@@ -0,0 +1,53 @@
+class Instruccion:
+    '''Clase abs de instrucciones'''
+
+
+class Imprimir(Instruccion):
+
+    def __init__(self,  cad) :
+        self.cad = cad
+
+class Declaracion(Instruccion):
+    def __init__(self, id, exp):
+        self.id = id
+        self.exp = exp
+
+class Asignacion(Instruccion):
+    def __init__(self, id, exp):
+        self.id = id
+        self.exp = exp
+
+class If(Instruccion):
+    def __init__(self, expLogica, instrucciones = []):
+        self.expLogica = expLogica
+        self.instrucciones = instrucciones
+
+class IfElse(Instruccion):
+    def __init__(self, expLogica, instrIfVerdadero = [], instrIfFalso = []):
+        self.expLogica = expLogica
+        self.instrIfVerdadero = instrIfVerdadero
+        self.instrIfFalso = instrIfFalso
+
+class While(Instruccion):
+    def __init__(self, expLogica, instrucciones = []):
+        self.expLogica = expLogica
+        self.instrucciones = instrucciones
+
+class Function(Instruccion):
+    def __init__(self, id, parametros = [], instrucciones = []):
+        self.id = id
+        self.parametros = parametros
+        self.instrucciones = instrucciones
+
+class CallFunction(Instruccion):
+    def __init__(self, id, params=[]):
+        self.id = id
+        self.params = params
+
+class ReturnInstr(Instruccion):
+    def __init__(self, exp):
+        self.exp = exp
+
+class BreakInstr(Instruccion):
+    def __init__(self) -> None:
+        super().__init__()
